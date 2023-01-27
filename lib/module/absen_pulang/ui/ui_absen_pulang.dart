0c98c3cce8b1cacce3b92f1dc90c333535027a78
nabilareyna <nabilareynarahmadhani@gmail.com>
2023-01-27 08:58:46 +0700

    add appbar

--- lib/module/absen_pulang/ui/ui_absen_pulang.dart
+++ lib/module/absen_pulang/ui/ui_absen_pulang.dart
@@ -1,15 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:sas/module/absen_pulang/controller/c_absen_pulang.dart';
-import 'package:sas/module/form_izin/controller/c_form_izin.dart';
+import 'package:sas/routes/routes.dart';
+import 'package:sas/utils/AppThemes.dart';
 
 class UIAbsenPulang extends GetView<CAbsenPulang> {
   const UIAbsenPulang({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Absen Pulang', style: Styles.appBarTitle),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Styles.secondaryColor,
+          ),
+          onPressed: () => Get.toNamed(Routes.dashboard),
+        ),
+      ),
+    );
   }
 }
--- lib/module/form_izin/ui/ui_form_izin.dart
+++ lib/module/form_izin/ui/ui_form_izin.dart
@@ -1,14 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:sas/module/form_izin/controller/c_form_izin.dart';
+import 'package:sas/routes/routes.dart';
+import 'package:sas/utils/AppThemes.dart';
 
 class UIFormIzin extends GetView<CFormIzin> {
   const UIFormIzin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Pengajuan Izin', style: Styles.appBarTitle),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Styles.secondaryColor,
+          ),
+          onPressed: () => Get.toNamed(Routes.dashboard),
+        ),
+      ),
+    );
   }
 }
