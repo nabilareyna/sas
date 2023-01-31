f88b4ae5678001ddaae6b269e69abbcd8b2c9c13
Fajar Ainur Roziqin <fajarainurrroziqin@gmail.com>
2023-01-31 18:06:26 +0700

    izin

--- lib/module/form_izin/ui/ui_form_izin.dart
+++ lib/module/form_izin/ui/ui_form_izin.dart
@@ -28,96 +28,173 @@ class UIFormIzin extends GetView<CFormIzin> {
       ),
       body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 25, left: 15, right: 15),
-            child: Column(
-              children: const [
-                Text(
-                  'Pilih Jenis Izin :',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 100),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 40,
-              decoration: const BoxDecoration(
-                color: Color(0xFFE7EFF1),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5.0),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 25, left: 15, right: 15),
+                child: Column(
+                  children: const [
+                    Text(
+                      'Pilih Jenis Izin :',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              // child: Row(
-              //   children: [
-              //     DropdownButton(
-              //       elevation: 10,
-              //       value: controller.selectedValue.value,
-              //       onChanged: (String? value) =>
-              //           controller.selectedValue.value = value,
-              //       items: controller.items
-              //           .map((e) => DropdownMenuItem(value: e, child: Text(e)))
-              //           .toList(),
-              //       style: const TextStyle(
-              //           fontFamily: 'Roboto',
-              //           fontSize: 12,
-              //           fontWeight: FontWeight.w400,
-              //           color: Colors.black,
-              //           letterSpacing: 1),
-              //       isDense: true,
-              //       hint: const Text('Pilih...'),
-              //       underline: const SizedBox(),
-              //       icon: const Icon(Icons.expand_more),
-              //     )
-              //   ],
-              // ),
-            ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 300),
-            child: GestureDetector(
-              onTap: () {
-                showDatePicker(
-                    context: context,
-                    locale: const Locale("id", "ID"),
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2018),
-                    lastDate: DateTime(2030),
-                    builder: (BuildContext context, Widget? child) {
-                      return Theme(
-                        data: ThemeData.dark(),
-                        child: child!,
-                      );
-                    });
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 40,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFE7EFF1),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.0),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, top: 50),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE7EFF1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Izin',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 11,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Icon(
+                          Icons.arrow_drop_down,
+                          size: 24.0,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE7EFF1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Tanggal Mulai*',
+                          style: TextStyle(
+                            color: Color(0xFFA3A3A3),
+                            fontSize: 11,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        ImageIcon(
+                          AssetImage('../assets/icons/datepicker.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE7EFF1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Tanggal Selesai*',
+                          style: TextStyle(
+                            color: Color(0xFFA3A3A3),
+                            fontSize: 11,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        ImageIcon(
+                          AssetImage('../assets/icons/datepicker.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE7EFF1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(right: 12),
+                          child: ImageIcon(
+                            AssetImage('../assets/icons/upload.png'),
+                          ),
+                        ),
+                        Text(
+                          'Upload Surat Izin',
+                          style: TextStyle(
+                            color: Color(0xFFA3A3A3),
+                            fontSize: 11,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: InkWell(
+                  child: const WButtonFormIzin(),
+                  onTap: () {
+                    Get.toNamed(Routes.getDashboardRoute());
+                  },
+                ),
+              )
+            ],
           ),
-          // Padding(
-          //   padding: const EdgeInsets.only(bottom: 20),
-          //   child: InkWell(
-          //     child: const WButtonFormIzin(),
-          //     onTap: () {
-          //       Get.toNamed(Routes.getDashboardRoute());
-          //     },
-          //   ),
-          // )
         ],
       ),
     );
