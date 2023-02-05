33ffa3eeb9f02c56716d51fd29e532182c13883d
Fajar Ainur Roziqin <fajarainurrroziqin@gmail.com>
2023-02-05 16:32:04 +0700

    pembaruan izin #1

--- lib/module/form_izin/widget/w_button_form_izin.dart
+++ lib/module/form_izin/widget/w_button_form_izin.dart
@@ -9,24 +9,28 @@ class WButtonFormIzin extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 15, top: 180),
+      padding: const EdgeInsets.only(left: 15, right: 15),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Container(
-            height: 40,
-            width: size.width,
-            decoration: BoxDecoration(
-                color: Styles.primaryColor,
-                borderRadius: BorderRadius.circular(10)),
-            child: const Center(
-              child: Text(
-                'Kirim',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 2),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 115),
+            child: Container(
+              height: 40,
+              width: size.width,
+              decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(5)),
+              child: const Center(
+                child: Text(
+                  'Kirim',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 2),
+                ),
               ),
             ),
           ),
